7ecca6a15e4639e624c121caf169e8f5012a1291
Sohamthesupercoder <yalamanchili.soham@gmail.com>
2025-07-19 12:48:01 +0530

    Changes for showing the page
    changes for showing the page

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/micro-cosmos-explorer-view/",
+  plugins: [react()],
+  base: '/micro-cosmos-explorer-view/',
   server: {
     host: "::",
     port: 8080,
