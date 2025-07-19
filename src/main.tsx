5b58fc0edc2c9bf234359a799153aa25621da315
Sohamthesupercoder <73933480+Sohamthesupercoder@users.noreply.github.com>
2025-07-19 14:16:44 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-types {
-    text/javascript js;
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
